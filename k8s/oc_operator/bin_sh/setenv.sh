8f8f9a0391ad699d15348428d2ec610e012d56f0
Dae Song Park <dspark@netcrest.com>
2020-09-03 15:31:53 -0400

    Added support for default storage

---
+++ k8s/oc_operator/bin_sh/setenv.sh
@@ -0,0 +1,24 @@
+#
+# Enter app specifics in this file.
+#
+
+# Cluster level variables:
+# ------------------------
+# BASE_DIR - padogrid base dir
+# ETC_DIR - Cluster etc dir
+
+# App level variables:
+# --------------------
+# APPS_DIR - <padogrid>/apps dir
+# APP_DIR - App base dir
+# APP_ETC_DIR - App etc dir
+
+#
+# Follow the instructions in README.md using the following environment variable.
+#
+export HAZELCAST_OPENSHIFT_DIR=$PADOGRID_WORKSPACE/k8s/oc_operator
+
+#
+# Default Kubernetes (OpenShift) project name.
+#
+export PROJECT_NAME="oc-operator"
